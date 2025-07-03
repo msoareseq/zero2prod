c91f97d0b46edb531d0d5f8fb3257ff4b2d5a0ea
Matheus Soares <69659304+msoareseq@users.noreply.github.com>
2025-07-03 00:37:37 -0300

    Chapter 3 final

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,7 @@
-use actix_web::{App, HttpRequest, HttpServer, Responder, web, HttpResponse};
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    let listener = std::net::TcpListener::bind("127.0.0.1:8000").expect("failed to bind to a address");
+    run(listener)?.await
 }
